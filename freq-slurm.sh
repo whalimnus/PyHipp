ffadfc601dadbda2e8bc8dc3bd1f061c8d90bfe0
AWS ParallelCluster user <ec2-user@ip-10-0-1-139.ap-southeast-1.compute.internal>
2022-10-31 14:02:54 +0000

    lab7

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -6,21 +6,16 @@
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1	# number of processors per task
-#SBATCH -J "rse"   # job name
+#SBATCH -J "freqslurm"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rse-slurm.%N.%j.out # STDOUT
-#SBATCH -e rse-slurm.%N.%j.err # STDERR
+#SBATCH -o freq-slurm.%N.%j.out # STDOUT
+#SBATCH -e freq-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
-import os; \
 import time; \
-t0 = time.time(); \
-print(time.localtime()); \
-os.chdir('sessioneye'); \
-pyh.RPLSplit(SkipLFP=False, SkipHighPass=False); \
-print(time.localtime()); \
-print(time.time()-t0);"
-
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:489176846228:awsnotify --message "RSEJobDone"
+pyh.RPLLFP(saveLevel=1); \
+pyh.FreqSpectrum(saveLevel=1); \
+pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
+print(time.localtime());"
